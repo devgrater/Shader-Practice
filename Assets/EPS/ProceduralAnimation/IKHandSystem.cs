fe1a026d2da42d923b2f214d913eb3dce6d8f0a1
DeveloGrater <Alligrater@outlook.com>
2022-02-23 16:10:46 -0500

    ik system

--- Assets/EPS/ProceduralAnimation/IKHandSystem.cs
+++ Assets/EPS/ProceduralAnimation/IKHandSystem.cs
@@ -5,37 +5,79 @@ using UnityEngine;
 public class IKHandSystem : MonoBehaviour
 {
     [System.Serializable]
-    public struct handSegment{
-        [SerializeField] int handLength;
-        [SerializeField] bool isConstrained;
+    public struct HandSegment{
+        [SerializeField] public int handLength;
+        [SerializeField] public bool isConstrained;
         //If this value is assigned, use this prefab instead of the default prefab.
-        [SerializeField] GameObject specialPrefab; 
+        [SerializeField] public GameObject specialPrefab; 
     }
     [Header("Hand Info")]
     [Tooltip("The default prefab to instantiate when generated.")]
     [SerializeField] private GameObject defaultPrefab;
 
     // Start is called before the first frame update
-    [SerializeField] public handSegment[] segmentDetails;
+    [SerializeField] private HandSegment[] segmentDetails;
+    [SerializeField] private Transform targetTransform;
     private List<IKHandInfo> instantiatedIKs;
 
 
     void InitializeIKSystem(){
+        //IKHandInfo previous = null;
+        instantiatedIKs = new List<IKHandInfo>();
         for(int i = 0; i < segmentDetails.Length; i++){
+            //create new ik game object
 
+            HandSegment segment = segmentDetails[i];
+            IKHandInfo handInfo = new IKHandInfo(segment.handLength);//worry about this later
+            ///previous = handInfo;
+            GameObject instantiated;
+            if(segment.specialPrefab){
+                instantiated = Instantiate(segment.specialPrefab);
+            }
+            else{
+                instantiated = Instantiate(defaultPrefab);
+            }
+            handInfo.SetGameObject(instantiated);
+            instantiatedIKs.Add(handInfo);
         }
     }
 
     void Start()
-    {
-        
+    {  
+        InitializeIKSystem();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //for each of the hands...
+        //target towards the next joint.
+        if(instantiatedIKs.Count <= 0) return;
+        Debug.Log(instantiatedIKs.Count);
+        Vector3 previousHead = new Vector3(0, 0, 0);
+        int i;
+        for(i = instantiatedIKs.Count - 1; i >= 0; i--){
+            IKHandInfo currentSegment = instantiatedIKs[i];
+            if(i == instantiatedIKs.Count - 1){
+                //this is the last segment in the hand system
+                currentSegment.RotateTowards(targetTransform.position);
+            }
+            else{
+                currentSegment.RotateTowards(previousHead);
+            }
+            previousHead = currentSegment.GetJointBase();
+        }
+        //Once everything is done,
+        //we backwards propagate everything back to the origin.
+        Vector3 backwardsPropagateVector = instantiatedIKs[0].GetJointBase() - transform.position;
+        for(i = 0; i < instantiatedIKs.Count; i++){
+            IKHandInfo currentSegment = instantiatedIKs[i];
+            currentSegment.ApplyConstraintOffset(backwardsPropagateVector);
+            currentSegment.UpdateGameObject();
+        }
     }
+
+
 }
 
 public sealed class IKHandInfo : IJointables
@@ -44,12 +86,28 @@ public sealed class IKHandInfo : IJointables
     private Vector3 forwardDirection;
     private float length;
     private GameObject bindedRenderObject;
-    public IKHandInfo(float length, GameObject bindedObject){
+    public IKHandInfo(float length){
         this.length = length;
-        this.bindedRenderObject = bindedObject;
+        //this.bindedRenderObject = bindedObject;
     }
 
-    void RotateTowards(Vector3 targetPoint){
+    public IKHandInfo(IKHandSystem.HandSegment segment){
+        this.length = segment.handLength;
+        //this.bindedRenderObject = GameObject.Instantiate()
+    }
+
+    public void SetGameObject(GameObject instantiated){
+        this.bindedRenderObject = instantiated;
+    }
+
+    public void UpdateGameObject(){
+        if(bindedRenderObject){
+            bindedRenderObject.transform.position = basePosition;
+            bindedRenderObject.transform.forward = forwardDirection;
+        }
+    }
+
+    public void RotateTowards(Vector3 targetPoint){
         Vector3 targetVector = targetPoint - GetJointBase();
         forwardDirection = targetVector;
         targetVector = targetVector.normalized * -length;
@@ -65,6 +123,10 @@ public sealed class IKHandInfo : IJointables
 
     public Vector3 GetJointEnd(){
         return basePosition + forwardDirection * length;
+    }
+
+    public void ApplyConstraintOffset(Vector3 offset){
+        basePosition -= offset;
     }
 
 }

29a945f0df0fc2945161163f272f516857d71e07
Alligrater <Alligrater@outlook.com>
2022-02-27 17:50:26 -0500

    setup

---
+++ Assets/EPS/ComputeShader/RampFog.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, RequireComponent(typeof(Camera)), ImageEffectAllowedInSceneView]
+public class RampFog : MonoBehaviour
+{
+    [Header("Base Settings")]
+    [SerializeField] private Camera targetCamera;
+
+    [Header("Colors")]
+    public Gradient gradient = new Gradient();
+    private Texture2D gradientMap;
+    [SerializeField] private Material postProcessMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void RecomputeGradientMap(){
+        //no mip
+        gradientMap = new Texture2D(256, 1, TextureFormat.ARGB32, false);
+        for(int i = 0; i < 256; i++){
+            gradientMap.SetPixel(i, 0, gradient.Evaluate(i / 256.0f));
+        }
+        gradientMap.Apply();
+        gradientMap.wrapMode = TextureWrapMode.Clamp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Application.isPlaying){
+            // The script is executing inside the editor
+            RecomputeGradientMap();
+        }
+    }
+
+    void UpdateMaterialParams(){
+            if(targetCamera == null){
+                targetCamera = GetComponent<Camera>();
+            }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest){
+        //regardless, you need to pass in some data...
+        if(!Application.isPlaying){
+            // The script is executing inside the editor
+            RecomputeGradientMap();
+        }
+ 
+
+        UpdateMaterialParams();
+        //command buffer
+        Graphics.Blit(src, dest, postProcessMat);
+    }
+}

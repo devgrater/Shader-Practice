f88c77a72ded701637cefc116e3915c66f69f86d
Alligrater <Alligrater@outlook.com>
2022-03-01 15:48:52 -0500

    paintbrush

--- Assets/EPS/ComputeShader/TraversePainter.cs
+++ Assets/EPS/ComputeShader/TraversePainter.cs
@@ -6,6 +6,9 @@ using UnityEditor;
 public class TraversePainter : MonoBehaviour
 {
     [SerializeField] private Camera targetCamera;
+    [SerializeField] private Collider targetCollider;
+    [SerializeField] private GameObject targetQuad;
+    [SerializeField] private GameObject paintBrushHead;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,9 @@ public class TraversePainter : MonoBehaviour
     void Update()
     {
         //raycast...
-        if(Input.GetButtonDown("Fire1")){
-            PaintAtPosition();
-        }
+        //if(Input.GetButtonDown("Fire1")){
+        PaintAtPosition();
+        //}
     }
 
     void PaintAtPosition(){
@@ -26,11 +29,43 @@ public class TraversePainter : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         //Debug.Log(mousePos);
         Ray targetRay = targetCamera.ScreenPointToRay(mousePos);
+        //shoot the ray!
+        RaycastHit hitResult;
+        if(Physics.Raycast(targetRay, out hitResult, 9999.0f)){
+            if(hitResult.collider == GetComponent<Collider>()){
+                //execute next step:
+                Vector2 hitUV = hitResult.textureCoord;
+                SetBrushHeadPos(hitUV);
+            }
+        }
     }
 
+    void SetBrushHeadPos(Vector2 uv){
+        if(paintBrushHead){
+            uv.x = (uv.x - 0.5f);
+            uv.y = (uv.y - 0.5f);
+            //set the position relative... to the camera.
+            Vector3 headPos = paintBrushHead.transform.position;
+            if(targetQuad != null){
+                float size = targetQuad.transform.localScale.x;
+                Vector3 camPos = targetQuad.transform.position;
+                //use the xy coordinates of this one.
+                Vector3 brushPos = new Vector3(camPos.x, camPos.y, headPos.z);
+                brushPos.x += uv.x * size;
+                brushPos.y += uv.y * size;
+                paintBrushHead.transform.position = brushPos;
+            }
+        }
+    }
+
+
+    void SaveRT(){
+
+    }
+    /*
     [MenuItem("GameObject/Setup Paint Canvas")]
     static void BeginPaint(){
 
-    }
+    }*/
 
 }
